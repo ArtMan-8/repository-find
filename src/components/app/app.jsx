3c328dc64d4e758a45a7d4ca2a6acc739c623bd5
Art <ya.Artriches@ya.ru>
2020-08-29 19:05:01 +0300

    init: подготовка окружения - webpack - react - eslint - prettier - babel

---
+++ src/components/app/app.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./app.css";
+
+export default function App() {
+  return <h1 className="app">App Component</h1>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { main: './src/index.jsx' },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: './js/bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: true,
+            }
+          }
+        ]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
