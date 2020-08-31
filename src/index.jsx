f155cad6fd8b866b5091aee679810d9fb0916768
Art <ya.Artriches@ya.ru>
2020-08-31 20:03:13 +0300

    feat: подключает Appolo

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,23 @@
+/* eslint-disable */
 import React from "react";
 import ReactDom from "react-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import App from "./components/app/app";
 
 require("./index.css");
+require("../.env");
 
-ReactDom.render(<App />, document.querySelector("#root"));
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  headers: {
+    authorization: GITHUB_GRAPHQL_API_KEY,
+  },
+  cache: new InMemoryCache(),
+});
+
+ReactDom.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.querySelector("#root")
+);
