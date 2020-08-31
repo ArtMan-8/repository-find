5876af0ed902d6e15bb59abcc754ce210ea694f2
Art <ya.Artriches@ya.ru>
2020-08-31 20:11:04 +0300

    add: заготовку для списка репозиториев, компанент Repos

---
+++ src/components/repos/repos.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./repos.css";
+
+export default function Repos({ data }) {
+  const repos = data && data.search.nodes;
+
+  const getStrDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const strDate = `${date.getDate()} / ${
+      date.getMonth() + 1
+    } / ${date.getFullYear()}`;
+    return strDate;
+  };
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Repository</th>
+            <th>Stars</th>
+            <th>Last Commit</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {repos &&
+            repos.map((repo) => (
+              <tr key={repo.url}>
+                <td>
+                  <a href="/#">{repo.name}</a>
+                </td>
+                <td>{repo.stargazers.totalCount}</td>
+                <td>
+                  {repo.ref
+                    ? getStrDate(repo.ref.target.committedDate)
+                    : `error`}
+                </td>
+                <td>
+                  <a href={repo.url}>link</a>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
