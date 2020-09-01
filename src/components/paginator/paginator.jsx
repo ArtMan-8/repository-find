db2aa6ae8869a3bf952375d9e57b84a75f33e862
Art <ya.Artriches@ya.ru>
2020-09-01 20:30:57 +0300

    add: заготовку компанент Paginator

--- src/components/paginator/paginator.jsx
+++ src/components/paginator/paginator.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import "./paginator.css";
 
 export default function Paginator() {
-  return <h1>Paginator Component</h1>;
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>10</li>
+    </ul>
+  );
 }
