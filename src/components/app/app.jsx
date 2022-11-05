9229d140156bc3f94e734edbc6a6146dfc2e6266
Art <ya.Artriches@ya.ru>
2022-11-05 19:41:49 +0300

    update: обновил сборщик на parcel

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Main from "../main";
 import Card from "../card";
-import "./app.css";
+import "./app.module.css";
 
 export default function App() {
-  return (
-    <HashRouter>
-      <Switch>
-        <Route path="/" component={Main} exact />
-        <Route
-          path="/:id"
-          render={(routeProps) => {
-            return <Card props={routeProps.match.params.id} />;
-          }}
-        />
-      </Switch>
-    </HashRouter>
-  );
+	return (
+		<HashRouter>
+			<Switch>
+				<Route path="/" component={Main} exact />
+				<Route
+					path="/:id"
+					render={(routeProps) => {
+						return <Card props={routeProps.match.params.id} />;
+					}}
+				/>
+			</Switch>
+		</HashRouter>
+	);
 }
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,59 +6,59 @@ import Repos from "../repos";
 import Paginator from "../paginator";
 import SearchContext from "../context";
 import getReposByPages from "../../utils/getRepoByPages";
-import styles from "./main.css";
+import * as styles from "./main.module.css";
 
 export default function Main() {
-  const lastRequest = sessionStorage.getItem("lastRequest");
-  const lastPage = sessionStorage.getItem("lastPage");
+	const lastRequest = sessionStorage.getItem("lastRequest");
+	const lastPage = sessionStorage.getItem("lastPage");
 
-  const [requestRepo, setRequestRepo] = React.useState(lastRequest);
-  const [currentPage, setPage] = React.useState(+lastPage || 1);
+	const [requestRepo, setRequestRepo] = React.useState(lastRequest);
+	const [currentPage, setPage] = React.useState(+lastPage || 1);
 
-  const setRequest = (currentRequest) => {
-    sessionStorage.setItem("lastRequest", currentRequest);
-    setRequestRepo(currentRequest);
-  };
+	const setRequest = (currentRequest) => {
+		sessionStorage.setItem("lastRequest", currentRequest);
+		setRequestRepo(currentRequest);
+	};
 
-  const setPageRepo = (page) => {
-    sessionStorage.setItem("lastPage", page);
-    setPage(page);
-  };
+	const setPageRepo = (page) => {
+		sessionStorage.setItem("lastPage", page);
+		setPage(page);
+	};
 
-  const { loading, error, data } = lastRequest
-    ? getQuery(GET_REPOS, {
-        variables: {
-          getQuery: `${requestRepo} sort:stars`,
-        },
-      })
-    : getQuery(TOP_REPOS);
+	const { loading, error, data } = lastRequest
+		? getQuery(GET_REPOS, {
+				variables: {
+					getQuery: `${requestRepo} sort:stars`,
+				},
+		  })
+		: getQuery(TOP_REPOS);
 
-  if (error) {
-    const str = `${error}`;
-    return <h2 className={styles.h2_error}>{str}</h2>;
-  }
+	if (error) {
+		const str = `${error}`;
+		return <h2 className={styles.h2_error}>{str}</h2>;
+	}
 
-  const pages =
-    data && getReposByPages(data.search.repositoryCount, data.search.nodes);
+	const pages =
+		data && getReposByPages(data.search.repositoryCount, data.search.nodes);
 
-  return (
-    <SearchContext.Provider value={[requestRepo, setRequest]}>
-      <Search />
+	return (
+		<SearchContext.Provider value={[requestRepo, setRequest]}>
+			<Search />
 
-      {loading ? (
-        <h3 className={styles.h3}>Loading...</h3>
-      ) : (
-        <>
-          <Repos repos={pages.repos[currentPage - 1]} />
-          {pages.count > 1 && (
-            <Paginator
-              pages={pages.count}
-              currentPage={currentPage}
-              setPage={setPageRepo}
-            />
-          )}
-        </>
-      )}
-    </SearchContext.Provider>
-  );
+			{loading ? (
+				<h3 className={styles.h3}>Loading...</h3>
+			) : (
+				<>
+					<Repos repos={pages.repos[currentPage - 1]} />
+					{pages.count > 1 && (
+						<Paginator
+							pages={pages.count}
+							currentPage={currentPage}
+							setPage={setPageRepo}
+						/>
+					)}
+				</>
+			)}
+		</SearchContext.Provider>
+	);
 }
