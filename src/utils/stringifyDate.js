671e2083ae8cf5a83dbb22f9500e7227e8337a81
Vladimir  Artyukhov <ya.Artriches@ya.ru>
2022-11-06 16:17:39 +0300

    Delete src directory

--- src/utils/stringifyDate.js
+++
@@ -1,7 +0,0 @@
-export default function stringifyDate(timestamp) {
-  const date = new Date(timestamp);
-  const strDate = `${date.getDate()} / ${
-    date.getMonth() + 1
-  } / ${date.getFullYear()}`;
-  return strDate;
-}
